8d228c1475231fe1b93afa2fb8a9c0b53bd4841c
Tiago Lopes <tiago.dweb@gmail.com>
2021-05-26 22:09:18 -0300

    test push

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require('./index.js')
 
 const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Example app listening at => http://localhost:${port}`))
